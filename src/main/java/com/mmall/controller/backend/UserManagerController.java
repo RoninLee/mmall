4810d1b0a043a68cf8aa3cfd7f3bcbe685ea9c6c
RoninLee <ITMaster233@gmail.com>
2018-09-11 22:50:25 +0800

    新建后台管理员controller

---
+++ src/main/java/com/mmall/controller/backend/UserManagerController.java
@@ -0,0 +1,11 @@
+package com.mmall.controller.backend;
+
+/**
+ * @program: mmall
+ * @author: RoninLee
+ * @Email: zlli233@qq.com
+ * @create: 2018-09-11 22:49
+ * @description: 后台管理员用户
+ **/
+public class UserManagerController {
+}
