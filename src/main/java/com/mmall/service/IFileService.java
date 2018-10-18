3cecc5e829433b5036bb273687d13b4e03e35862
RoninLee <ITMaster233@gmail.com>
2018-10-18 23:11:49 +0800

    新增文件上传服务

---
+++ src/main/java/com/mmall/service/IFileService.java
@@ -0,0 +1,11 @@
+package com.mmall.service;
+
+/**
+ * @program: mmall
+ * @author: RoninLee
+ * @Email: zlli233@qq.com
+ * @create: 2018-10-17 19:01
+ * @description: 文件处理的服务
+ **/
+public interface IFileService {
+}
---
+++ src/main/java/com/mmall/service/Impl/FileServiceImpl.java
@@ -0,0 +1,11 @@
+package com.mmall.service.Impl;
+
+/**
+ * @program: mmall
+ * @author: RoninLee
+ * @Email: zlli233@qq.com
+ * @create: 2018-10-17 19:04
+ * @description: 文件处理服务的实现类
+ **/
+public class FileServiceImpl {
+}
