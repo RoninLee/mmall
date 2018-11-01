fc6e6b555304148af51ad492a523bf501bfa74b3
RoninLee <ITMaster233@gmail.com>
2018-11-01 22:17:49 +0800

    前台的产品管理Controller以及Service

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -12,6 +12,7 @@ import java.util.Set;
  * @description: 常量类
  **/
 public class Const {
+
     public static final String CURRENT_USER = "currentUser";
     public static final String EMAIL = "email";
     public static final String USERNAME = "username";
