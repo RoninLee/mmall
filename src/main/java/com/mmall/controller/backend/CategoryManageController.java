24a8914abe13504d04f062bcfd1ebecfaa8d1837
RoninLee <ITMaster233@gmail.com>
2018-11-25 23:37:12 +0800

    test

--- src/main/java/com/mmall/controller/backend/CategoryManageController.java
+++ src/main/java/com/mmall/controller/backend/CategoryManageController.java
@@ -24,6 +24,7 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/Manage/category/")
 public class CategoryManageController {
+
     @Autowired
     private IUserService iUserServer;
     @Autowired
